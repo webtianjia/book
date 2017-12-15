75fe2128be27b51b519d1095f9ea11b167a230e8
tianjia <676854227@qq.com>
2017-12-15 09:06:54 +0800

    1212

---
+++ src/components/list-footer/list-footer.vue
@@ -0,0 +1,34 @@
+<style scoped lang="less">
+  .list-footer {
+    display: flex;
+    a {
+      flex: 1;
+      display: block;
+      padding: 14px;
+      font: 13px/1.3em a;
+      text-align: center;
+      color: rgba(0, 0, 0, .9);
+      box-sizing: border-box;
+      &:first-child {
+        border-right: 1px solid #f0f0f0;
+      }
+    }
+  }
+</style>
+<template>
+  <footer class="list-footer">
+    <a href="javascript:">换一换</a>
+    <a href="javascript:">查看全部</a>
+  </footer>
+</template>
+<script>
+  export default {
+    props:{
+
+    },
+    data () {
+      return {}
+    },
+    methods: {}
+  }
+</script>
---
+++ src/components/list-heading/list-heading.vue
@@ -0,0 +1,88 @@
+<style lang="less" scoped>
+  @import "../../assets/css/mixin";
+
+  .list-heading {
+    padding: 15px 13px 14px 13px;
+    .border-1px(#f0f0f0);
+    position: relative;
+    .title {
+      position: relative;
+      font: bold 13px/13px a;
+      color: rgba(0, 0, 0, .9);
+      i {
+        position: absolute;
+        margin: -1px 0 0 5px;
+        padding: 3px 3px 0px 3px;
+        font: 9px/9px a;
+        color: #fff;
+        background-color: #53ac7d;
+        border-radius: 1px;
+      }
+    }
+    .tab {
+      position: absolute;
+      top: 9px;
+      right: 13px;
+      a {
+        position: relative;
+        padding: 16px 10px;
+        font: 12px/12px a;
+        color: #999;
+        &:first-child:after {
+          content: "";
+          position: absolute;
+          top: 16px;
+          bottom: 16px;
+          right: -2px;
+          width: 1px;
+          border-right: 1px solid #ccc;
+        }
+        &.active {
+          color: #528ae8;
+        }
+      }
+    }
+  }
+</style>
+<template>
+  <header class="list-heading border-1px">
+    <div class="title">{{title}} <i v-show="recommend">推</i></div>
+    <div class="tab" v-if="tab">
+      <a href="javascript:" @click="type=1" :class="{'active':type==1}">{{desc.positive}}</a>
+      <a href="javascript:" @click="type=2" :class="{'active':type==2}">{{desc.reverse}}</a>
+    </div>
+  </header>
+</template>
+<script>
+  export default {
+    props: {
+      tab: {
+        type: Boolean,
+        default: false
+      },
+      recommend: {
+        type: Boolean,
+        default: false
+      },
+      title: {
+        type: String,
+        required: true
+      },
+      desc: {
+        type: Object,
+        default () {
+          return {
+            positive: '男',
+            reverse: '女'
+          }
+        }
+      }
+    },
+    data () {
+      return {
+        type: 1
+      }
+    },
+    methods: {}
+  }
+</script>
